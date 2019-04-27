0a429f2c44b444e20cbb8c1ceddb2bc39f2a0b83
Jamie Haddow <jamiehaddow@hotmail.com>
2019-04-27 02:39:52 +0100

    asteroids

---
+++ Cgadius 0.67/CMP105App/AsteroidManager.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Asteroid.h"
+#include <math.h>
+#include <iostream>
+#include "Framework/Collision.h"
+#include "Framework/GameObject.h"
+#include <vector>
+#include "WallManager.h"
+#include "EnemyManager.h"
+#include "WallManager.h"
+
+class AsteroidManager
+{
+public:
+	AsteroidManager();
+	~AsteroidManager();
+
+	void spawn();
+	void update(float dt);
+	void deathCheck();
+	void render(sf::RenderWindow* window);
+	void setWallManager(WallManager* wm)
+	{
+		wallManager = wm;
+	}
+
+
+
+
+private:
+	std::vector<Asteroid> asteroids;
+	sf::Texture texture;
+	WallManager* wallManager;
+	sf::Vector2f asteroidICentre;
+	sf::Vector2f asteroidJCentre;
+};
